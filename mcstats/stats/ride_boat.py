b5cf485315f605accf5301dac52bff63bec8b422
TheLittle_Yang <2570984321@qq.com>
2020-08-11 00:07:04 +0800

    更新 ride_boat.py

--- mcstats/stats/ride_boat.py
+++ mcstats/stats/ride_boat.py
@@ -5,7 +5,7 @@ mcstats.registry.append(
         'ride_boat',
         {
             'title': '水手',
-            'desc': '用船航行的距离,
+            'desc': '用船航行的距离',
             'unit': 'cm',
         },
         mcstats.StatReader(['minecraft:custom','minecraft:boat_one_cm'])
